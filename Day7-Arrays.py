d0e395273e5b611b6ddc6dd0383c27296c8758ca
C L <ccaj007@gmail.com>
2022-07-16 17:25:03 -0400

    day 8

---
+++ Day7-Arrays.py
@@ -0,0 +1,4 @@
+N = int(input())
+A = input()
+
+print(A[::-1])
---
+++ day8-dictionarymaps.py
@@ -0,0 +1,15 @@
+phonebook = {}
+n = int(input())
+for _ in range(n):
+    contact = input().split(' ')
+    phonebook[contact[0]] = contact[1]
+
+for _ in range(n):
+    find_name = input()
+    if find_name in phonebook:
+        print(find_name + "=" + phonebook[find_name])
+    else:
+        print("Not found")
+#print(phonebook)
+
+

df6243bf64c335c62ef988cb2832015e8fa2466b
C L <ccaj007@gmail.com>
2022-07-16 16:46:24 -0400

    day 6

--- Day4ClassvsInstance.py
+++ Day4ClassvsInstance.py
@@ -3,8 +3,21 @@
 class Person:
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            print('you enter negative number, age will be 0')
+            self.age = 0
+        else:
+            self.age = initialAge
+
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        if (self.age < 13):
+            print('you are a child')
+        elif ( 13 <= self.age <= 18):
+            print('you are a teenager')
+        else:
+            print('you are old')
+
     def yearPasses(self):
         # Increment the age of the person in here
 

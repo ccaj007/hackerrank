65463733857408dcb2fed9bae9dda16ff0bb7b9a
C L <ccaj007@gmail.com>
2022-07-20 22:31:03 -0400

    day 9

--- day8-dictionarymaps.py
+++ day8-dictionarymaps.py
@@ -7,7 +7,7 @@ for _ in range(n):
 for _ in range(n):
     find_name = input()
     if find_name in phonebook:
-        print(find_name + "=" + phonebook[find_name])
+        print(find_name + "=" + str(phonebook[find_name]))
     else:
         print("Not found")
 #print(phonebook)

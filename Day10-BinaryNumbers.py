73403441d721970007c1aa42d032de2e7450eb7b
C L <ccaj007@gmail.com>
2022-07-30 18:54:13 -0400

    add

--- Day10-BinaryNumbers.py
+++ Day10-BinaryNumbers.py
@@ -6,3 +6,18 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
+    max_value = 0
+    tmp = 0
+
+    while n > 0:
+        if n % 2 == 1:
+            tmp += 1
+            if tmp > max_value:
+                max_value = tmp
+        else:
+            tmp = 0
+
+        n //=2
+    print(max_value)
+
+
